3375653f4734fa2b55b63283ad1cd3a676b1c57e
JacintaNgwu <jacintangwu9900@gmail.com>
2023-03-10 12:43:39 +0530

    created class and methods

---
+++ myList.rb
@@ -0,0 +1,30 @@
+require_relative  "./enumerables.rb" 
+
+class Mylist
+    include MyEnumerables
+    def initialize(*list)
+      @list = list
+    end
+  
+    def each(&block)
+      @list.each(&block)
+    end
+  end
+
+  list = Mylist.new(1, 2, 3, 4)
+
+# Test #all?
+irb> list.all? {|e| e < 5}
+=> true
+irb> list.all? {|e| e > 5}
+=> false
+
+# Test #any?
+irb> list.any? {|e| e == 2}
+=> true
+irb> list.any? {|e| e == 5}
+=> false
+
+# Test #filter
+irb> list.filter {|e| e.even?}
+=> [2, 4]
